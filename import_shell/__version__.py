6eed30243605362cbce7b61cf9445e94f50c3afa
Caceresenzo <caceresenzo1502@gmail.com>
2025-06-01 22:54:57 +0200

    release: bump to 0.3.0

--- import_shell/__version__.py
+++ import_shell/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'import-shell'
 __description__ = 'import-shell - Start testing your code with a quick command!'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'caceresenzo1502@gmail.com'
 __url__ = 'https://github.com/Caceresenzo/import-shell'
